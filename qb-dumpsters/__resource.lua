0b044fac14ebc368468da72aa1b302be15cff838
PhantomDDK <dododenmark@gmail.com>
2021-04-07 13:30:18 +0200

    Update __resource.lua

--- qb-dumpsters/__resource.lua
+++ qb-dumpsters/__resource.lua
@@ -1,5 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+description 'qb-dumpster made by PhantomDDK'
+version "1.0.4"
 
 client_scripts {
   
@@ -9,7 +11,7 @@ client_scripts {
 }
 
 server_scripts {
-  
+  'sv_version_check.lua',
   'server/server.lua',
  
   'config.lua'
