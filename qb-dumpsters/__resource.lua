32fd914c5f6c9140f0fda0cae006b3eb45aa1bef
zWyzzzzz <42010572+zWyzzzzz@users.noreply.github.com>
2022-03-16 22:38:14 +0100

    Update __resource.lua

--- qb-dumpsters/__resource.lua
+++ qb-dumpsters/__resource.lua
@@ -4,16 +4,13 @@ description 'qb-dumpster made by PhantomDDK'
 version "1.0.4"
 
 client_scripts {
-  
-  'client/client.lua',
-  
+  'client/client.lua', 
   'config.lua'
 }
 
 server_scripts {
   'sv_version_check.lua',
   'server/server.lua',
- 
   'config.lua'
 }
 
